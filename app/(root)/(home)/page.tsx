3b985ee20bb57389d6e8132e5f19f36ffe55a75e
Anurag <anurag789p@gmail.com>
2025-02-03 23:12:57 +0530

    The theme is implemented --->> we have made both dark and light mode work and issolve the re-rendering or flickering.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,13 +10,13 @@
 
 // export default Home
 
-import { UserButton } from "@clerk/nextjs";
+
 
 
 export default function Home() {
 return (
   <div>
-    <UserButton afterSignOutUrl="/"/>
+
   </div>
 )
 }
