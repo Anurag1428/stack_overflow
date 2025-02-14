9bfbaef860333e493651ff881ab92507e23b7079
Anurag <anurag789p@gmail.com>
2025-02-14 19:38:08 +0530

    made our database and connected and made some models

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {},  // ✅ Enables server actions
+    mdxRs: true,        // ✅ Enables Rust-based MDX compiler
+  },
+  serverExternalPackages: ['mongoose'], // ✅ Use this instead of experimental.serverComponentsExternalPackages
 };
 
 export default nextConfig;
-
