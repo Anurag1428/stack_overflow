c69ec104454cc1a06bfd4a60f4b9f72eebca29ff
Anurag <anurag789p@gmail.com>
2025-02-16 15:51:00 +0530

    successfully created our first question-connected to mongodb - request is made from our local host

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -16,7 +16,7 @@ export const connectToDatabase = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URL, {
-            dbName: 'dev overflow'
+            dbName: 'devoverflow'
         })
 
         isConnected = true;
