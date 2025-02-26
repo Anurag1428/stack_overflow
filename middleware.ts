8d69f382b23469dbc6bccb31822449bbf3990c28
Anurag <anurag789p@gmail.com>
2025-02-26 22:37:20 +0530

    done some changes in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,55 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// const isPublicRoute = createRouteMatcher([
+//   '/',
+//   '/sign-in(.*)',
+//   '/sign-up(.*)',
+//   '/question/:id',
+//   '/tags',
+//   '/tags/:id',
+//   '/profile/:id',
+//   '/community',
+//   '/api/webhook'
+// ]);
+
+// const isIgnoredRoute = createRouteMatcher([
+//   '/api/chatgpt'
+// ]);
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request) && !isIgnoredRoute(request)) {
+//     await auth.protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Protect all API routes except explicitly ignored ones
+//     '/api/(auth|user|protected-route|trpc/.*)'
+//   ],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
+  '/api/webhook',
   '/question/:id',
   '/tags',
   '/tags/:id',
   '/profile/:id',
   '/community',
-  '/api/webhook'
-]);
-
-const isIgnoredRoute = createRouteMatcher([
-  '/api/chatgpt'
-]);
+  '/jobs'
+])
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request) && !isIgnoredRoute(request)) {
-    await auth.protect();
+  if (!isPublicRoute(request)) {
+    await auth.protect()
   }
-});
+})
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Protect all API routes except explicitly ignored ones
-    '/api/(auth|user|protected-route|trpc/.*)'
-  ],
-};
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
