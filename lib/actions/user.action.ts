0e4045c424bf68acba4540cc3969d44c5ea55f87
Anurag <anurag789p@gmail.com>
2025-03-05 23:39:59 +0530

    made our community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,9 +2,10 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { CreateUserParams, DeleteUserParams, GetAllUsersParams, UpdateUserParams } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import { log } from "node:console";
 
 export async function getUserById(params: any) {
   try {
@@ -135,3 +136,29 @@ export async function createOrUpdateUser(userData: CreateUserParams) {
     };
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({})
+      .sort({ createdAt: -1 })
+
+      return {users};
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams) {
+  //   try {
+  //     connectToDatabase();
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
