6929d96a5bdb6e5f340e7936335a1caea7826753
Anurag <anurag789p@gmail.com>
2025-03-12 17:40:55 +0530

    made our collections page

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -18,7 +18,7 @@ interface QuestionProps {
         name: string;
         picture: string;
      };
-     upvotes: number;
+     upvotes: string[];
      views: number;
      answers: Array<object>;
      createdAt: Date;
@@ -76,7 +76,7 @@ const QuestionCard = ({
             <Metric
                 imgUrl="/assets/icons/like.svg"
                 alt="Upvotes"
-                value={formatBigNumber(upvotes)}
+                value={formatBigNumber(upvotes.length)}
                 title=" Votes"
                 textStyles="small-medium text-dark400_light800"
             />
