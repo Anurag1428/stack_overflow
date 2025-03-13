038e6d1e33e239b0b1ac2627266b5c077f183134
Anurag <anurag789p@gmail.com>
2025-03-14 00:42:56 +0530

    started making our badges section

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -10,6 +10,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { getJoinedDate } from '@/lib/utils';
 import ProfileLink from '@/components/shared/ProfileLink';
 import Stats from '@/components/shared/Stats';
+import QuestionTab from '@/components/shared/QuestionTab';
 
 
 const Page = async ({ params, searchParams} : URLProps) => {
@@ -79,7 +80,10 @@ const Page = async ({ params, searchParams} : URLProps) => {
             </div>
         </div>
 
-        <Stats/>
+        <Stats
+            totalQuestions={userInfo.totalQuestions}
+            totalAnswers={userInfo.totalAnswers}
+        />
 
         <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
@@ -88,10 +92,10 @@ const Page = async ({ params, searchParams} : URLProps) => {
                 <TabsTrigger value="answers" className="tab">Answers</TabsTrigger>
             </TabsList>
             <TabsContent value="top-posts">
-                POSTS
+                <QuestionTab/>
             </TabsContent>
             <TabsContent value="answers">
-                ANSWERS
+                AnswersTab
             </TabsContent>
         </Tabs>
 
