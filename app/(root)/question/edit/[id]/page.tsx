9b31e1b3114426d7d6beec68bb42bfd1eca7d62d
Anurag <anurag789p@gmail.com>
2025-03-17 10:15:09 +0530

    created edit question page

--- app/(root)/question/edit/[id]/page.tsx
+++ app/(root)/question/edit/[id]/page.tsx
@@ -1,12 +1,36 @@
- import React from 'react'
+ import Question from '@/components/forms/Question';
+import { getQuestionById } from '@/lib/actions/question.action';
+import { getUserById } from '@/lib/actions/user.action';
+import { ParamsProps } from '@/types';
+import { auth } from '@clerk/nextjs/server';
+import { useParams } from 'next/navigation';
+import React from 'react'
  
- const page = () => {
+ const Page = async ({ params} : ParamsProps) => {
+  const { userId } = await auth();
+
+  if(!userId) return null;
+
+  const mongoUser = await getUserById({ userId});
+  const result = await getQuestionById({ questionId: params.id})
+
+
    return (
-     <div>
-       Page
-     </div>
+     <>
+       <h1 className="h1-bold text-dark100_light900">
+        Edit Question
+        </h1>
+
+        <div>
+          <Question
+          type="Edit"
+          mongoUserId={mongoUser._id}
+          questionDetails={JSON.stringify(result)}
+          />
+        </div>
+     </>
    )
  }
  
- export default page
+ export default Page;
  
